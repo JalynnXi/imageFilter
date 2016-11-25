b54212e988e965987874d36a02ad0f20b2b6bb1f
JalynnXi <xijx@kjmobi.com>
2016-11-25 10:22:12 +0800

    Initial Commit

---
+++ 图像滤镜学习/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  图像滤镜学习
+//
+//  Created by JalynnXi on 25/11/16.
+//  Copyright © 2016年 JalynnXi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 图像滤镜学习/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  图像滤镜学习
+//
+//  Created by JalynnXi on 25/11/16.
+//  Copyright © 2016年 JalynnXi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
